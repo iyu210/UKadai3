12571979665f48912624977de41a135f3b2e26c3
iyu210 <iyu@live.jp>
2022-01-17 00:34:34 +0900

    initial commit

--- UKadai3/ViewController.swift
+++ UKadai3/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var leftTextField: UITextField!
+    @IBOutlet weak var rightTextField: UITextField!
+    @IBOutlet weak var leftSwitch: UISwitch!
+    @IBOutlet weak var rightSwitch: UISwitch!
+    @IBOutlet weak var upperLabel: UILabel!
+    @IBOutlet weak var lowerLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        var num1 = Int(leftTextField.text ?? "") ?? 0
+        var num2 = Int(rightTextField.text ?? "") ?? 0
+        if leftSwitch.isOn == true {
+            num1 *= -1
+        }
+        if rightSwitch.isOn == true {
+            num2 *= -1
+        }
+        upperLabel.text = "\(num1)  +  \(num2)"
+        lowerLabel.text = "\(num1 + num2)"
+    }
 }
-
